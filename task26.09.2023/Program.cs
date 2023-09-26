c61707993961577dc5d479219186ad8d6c1b0ced
Your Name <you@example.com>
2023-09-27 01:42:49 +0400

    task elave olundu

---
+++ task26.09.2023/Program.cs
@@ -0,0 +1,67 @@
+﻿//private static void Main(string[] args)
+//{
+//    //Task1
+
+//    int number = 4511;
+
+//    int minlik = number/1000;
+
+//    int yuzluk = number % 1000 / 100;
+
+//    int onluq = number % 100 / 10;
+
+//    int teklik = number % 100 % 10;
+//    Console.WriteLine (minlik + yuzluk + onluq + teklik);
+//}
+
+//Task2
+
+//int number = 159;
+//    int yuzluk=number/100;
+//int onluq = number % 100 / 10;
+//int teklik=number % 10;
+//if (yuzluk!=onluq || yuzluk!=teklik || onluq!=teklik )
+//{
+//    Console.WriteLine("reqemler ferqlidir"  );
+//}
+//else
+//{
+//    Console.WriteLine(  "ferqli olmayan reqemler var");
+//}
+
+//Task 3
+
+//int a = 2;
+//int b = 3;
+//int c = a; 
+//a = b; 
+//b = c; 
+
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+
+
+//Task4
+
+//byte age = 60;
+//string person = "telebe";
+//if (age>=13 && age<60)
+//{
+//    if (person=="telebe")
+//    {
+//        Console.WriteLine(  "Telebe endirimi = 7.5 AZN");
+//    }
+//    else
+//    {
+//        Console.WriteLine(  "Standart qiymet = 10 AZN");
+//    }
+
+//}
+//else
+//{
+//    Console.WriteLine(  "Endirimli Qiymet = 5 AZN");
+//}
+
+
